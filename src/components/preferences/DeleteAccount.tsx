4d8a57bbece5e9b4ffe096f920bca80cdf498e30
matheus <matheus.felipe.19rt@gmail.com>
2025-02-25 01:05:43 -0300

    feat(ui, config): add DeleteAccount component, create presets for Carousel component and theming

---
+++ src/components/preferences/DeleteAccount.tsx
@@ -0,0 +1,37 @@
+import { useDeleteUser } from "@/src/hooks/useMutations"
+import { Icon } from "@iconify/react"
+
+export default function DeleteAccount() {
+	const { mutate: deleteUser, isPending, error } = useDeleteUser()
+
+	const handleDeleteAccount = () => {
+		const confirm = window.confirm("Are you sure you want to delete your account?")
+		if (!confirm) return false
+
+		deleteUser(undefined, {
+			onSuccess: () => {
+				window.location.href = "/"
+			},
+			onError: (err) => {
+				alert(error || err.message || "An error occurred while deleting the account.")
+			}
+		})
+	}
+
+	return (
+		<>
+			<header className="my-2">
+				<h2>Delete Account</h2>
+				<h6 className="text-danger">This action is irreversible. All data will be lost.</h6>
+			</header>
+			<div className="input-group justify-end md:justify-start">
+				<button onClick={handleDeleteAccount} title="Delete Account" className="btn bg-danger">
+					<Icon icon="mdi:user-remove" width={20} height={20} />
+					{isPending ? "Deleting..." : "Delete Account"}
+				</button>
+			</div>
+
+			{error && <div className="mt-2 text-danger">{error.message}</div>}
+		</>
+	)
+}

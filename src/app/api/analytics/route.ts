3d57c4e7b65f1d20f86f23bbe88cd14eadb1dde8
matheus <matheus.felipe.19rt@gmail.com>
2025-02-27 20:17:41 -0300

    feat(ui, api): add AnalyticsPanel & ClicksByLink components, implement analytics functionality

---
+++ src/app/api/analytics/route.ts
@@ -0,0 +1,60 @@
+import { db } from "@/src/lib/db"
+import { getSessionOrUnauthorized } from "@/src/lib/utils"
+import { NextRequest, NextResponse } from "next/server"
+
+// GET method for getting user analytics data
+export async function GET() {
+	const { error, session, response } = await getSessionOrUnauthorized()
+	if (error) return response
+
+	const analyticsData = await db.userStats.findMany({ where: { userId: session.user.id } })
+	if (!analyticsData) {
+		return NextResponse.json({ error: "Analytics data not found" }, { status: 404 })
+	}
+
+	return NextResponse.json(analyticsData, { status: 200 })
+}
+
+// POST method for processing link and social icon clicks
+export async function POST(req: NextRequest) {
+	const { type, id } = await req.json()
+
+	// Create a new linkClick or iconClick entry with the current date for every click and increment the clicks count
+	if (type === "link") {
+		try {
+			const linkClick = await db.linkClick.create({
+				data: {
+					userLinkId: id,
+					date: new Date()
+				}
+			})
+
+			await db.userLink.update({
+				where: { id },
+				data: { clicks: { increment: 1 } }
+			})
+
+			return NextResponse.json(linkClick, { status: 200 })
+		} catch {
+			return NextResponse.json({ error: "Failed to create link click" }, { status: 500 })
+		}
+	} else if (type === "icon") {
+		try {
+			const iconClick = await db.iconClick.create({
+				data: {
+					userIconId: id,
+					date: new Date()
+				}
+			})
+
+			await db.userIcon.update({
+				where: { id },
+				data: { clicks: { increment: 1 } }
+			})
+
+			return NextResponse.json(iconClick, { status: 200 })
+		} catch {
+			return NextResponse.json({ error: "Failed to create social icon click" }, { status: 500 })
+		}
+	}
+}
--- src/components/analytics/ClicksByLink.tsx
+++ src/components/analytics/ClicksByLink.tsx
@@ -1,3 +1,44 @@
+import { useGetClicksByLink } from "@/src/hooks/useQueries"
+import { formatDate } from "@/src/lib/utils"
+import { Icon } from "@iconify/react"
+
 export default function ClicksByLink() {
-	return <div></div>
+	const { data: items } = useGetClicksByLink()
+
+	return (
+		<>
+			<header className="my-2">
+				<h2>Clicks By Link</h2>
+				<h6 className="text-muted-foreground">Your most visited links & social icons.</h6>
+			</header>
+
+			{!items || items.length === 0 ? (
+				<h4 className="my-2 text-center text-muted-foreground">No links or social icons available yet.</h4>
+			) : (
+				<ul className="grid grid-cols-1 gap-2 md:grid-cols-3">
+					{items.map((item) => (
+						<li key={item.url} className="card">
+							{item.type === "link" ? (
+								<div className="mb-2 flex flex-row items-center gap-2">
+									<h4 className="truncate text-muted-foreground">{item.title}</h4>
+									<span className="whitespace-nowrap font-semibold"> - {item.clicks} clicks</span>
+								</div>
+							) : (
+								<div className="mb-2 flex flex-row items-center gap-2">
+									{item.icon && <Icon icon={item.icon} className="size-5 text-muted-foreground" />}
+									<h4 className="truncate text-muted-foreground">{item.platform}</h4>
+									<span className="whitespace-nowrap font-semibold"> - {item.clicks} clicks</span>
+								</div>
+							)}
+
+							<div className="flex flex-col gap-1">
+								<span className="text-xs text-muted-foreground">{item.url}</span>
+								<span className="text-xs text-muted-foreground">Created at {formatDate(item.createdAt)}</span>
+							</div>
+						</li>
+					))}
+				</ul>
+			)}
+		</>
+	)
 }

c73c8043407159ac64b7c7881bf718059ed832e5
Matheus Mortari <matheus.felipe.19rt@gmail.com>
2025-10-27 11:53:00 -0300

    refactor(api, state): standardize API endpoints and service calls

---
+++ server/api/analytics/index.get.ts
@@ -0,0 +1,29 @@
+import db from "#server/lib/db"
+import { getUserFromSession } from "#server/lib/utils"
+
+export default defineEventHandler(async (event) => {
+  const user = await getUserFromSession(event)
+
+  const [pageViews, linkClicks, iconClicks] = await Promise.all([
+    db.pageView.findMany({
+      where: { userId: user.id },
+      orderBy: { date: "desc" },
+    }),
+    db.linkClick.findMany({
+      where: { userLink: { userId: user.id } },
+      orderBy: { date: "desc" },
+      include: { userLink: true },
+    }),
+    db.iconClick.findMany({
+      where: { userIcon: { userId: user.id } },
+      orderBy: { date: "desc" },
+      include: { userIcon: true },
+    }),
+  ])
+
+  return {
+    pageViews,
+    linkClicks,
+    iconClicks,
+  }
+})
--- server/api/links/index.post.ts
+++ server/api/links/index.post.ts
@@ -3,15 +3,14 @@ import { getUserFromSession } from "#server/lib/utils"
 import { createUserLinkSchema } from "#shared/schemas/links"
 
 export default defineEventHandler(async (event) => {
-  const sessionUser = await getUserFromSession(event)
+  const user = await getUserFromSession(event)
 
   const body = await readBody(event)
-
   const { url, title } = createUserLinkSchema.parse(body)
 
   const newLink = await db.userLink.create({
     data: {
-      userId: sessionUser.id,
+      userId: user.id,
       url,
       title,
     },

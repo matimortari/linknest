fdcb3cbc1e293c30029d3ce57a3825ccbffa2872
Matheus Mortari <matheus.felipe.19rt@gmail.com>
2025-10-29 11:43:05 -0300

    refactor(ui, css): update layout and styles for sign in page

--- app/pages/sign-in.vue
+++ app/pages/sign-in.vue
@@ -1,23 +1,47 @@
 <template>
-  <div class="flex flex-col items-center justify-center gap-8">
-    <header class="my-4 flex flex-col items-center justify-center gap-2">
-      <h2>
-        Sign In
-      </h2>
-    </header>
+  <div class="relative flex min-h-screen">
+    <Navbar />
 
-    <div class="my-4 flex flex-col items-center gap-4">
-      <p class="text-muted-foreground text-lg font-semibold">
-        Choose a provider to continue.
-      </p>
+    <div class="z-10 flex w-full flex-col items-center justify-center px-6 py-12 md:w-1/2">
+      <div class="w-full max-w-md">
+        <header class="my-4 flex flex-col items-center gap-2 text-center">
+          <h1>
+            Sign In
+          </h1>
+          <p class="text-muted-foreground">
+            Choose a provider to continue.
+          </p>
+        </header>
 
-      <div class="flex flex-row items-center gap-4">
-        <button v-for="provider in OAUTH_PROVIDERS" :key="provider.name" class="btn" @click="signIn(provider.name)">
-          <icon :name="provider.icon" size="25" />
-          <span>{{ provider.label }}</span>
-        </button>
+        <div class="flex flex-col justify-center gap-4 p-4">
+          <button v-for="provider in OAUTH_PROVIDERS" :key="provider.name" class="btn" @click="signIn(provider.name)">
+            <icon :name="provider.icon" size="25" />
+            <span>{{ provider.label }}</span>
+          </button>
+        </div>
+
+        <footer class="mx-auto my-4 max-w-xs text-center">
+          <p class="text-muted-foreground text-center text-sm">
+            By signing in, you agree to our
+            <nuxt-link to="/legal/terms" class="hover:text-primary underline">
+              Terms of Service
+            </nuxt-link>
+            and
+            <nuxt-link to="/legal/privacy" class="hover:text-primary underline">
+              Privacy Policy
+            </nuxt-link>.
+          </p>
+        </footer>
       </div>
     </div>
+
+    <!-- Right side image for desktop -->
+    <div class="hidden md:block md:w-1/2">
+      <img src="/assets/sign-in-image.png" alt="Sign In Background" class="size-full object-cover">
+    </div>
+
+    <!-- Fullscreen image for mobile -->
+    <img src="/assets/sign-in-image.png" alt="Sign In Background" class="absolute inset-0 -z-10 block size-full object-cover opacity-20 md:hidden">
   </div>
 </template>
 
@@ -29,5 +53,9 @@ function signIn(provider: string) {
 useHead({
   title: "Sign In",
   meta: [{ name: "description", content: "LinkNest Sign In page" }],
+})
+
+definePageMeta({
+  layout: "fullscreen",
 })
 </script>

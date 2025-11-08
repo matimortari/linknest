5e7848185b1c159e218aa4911861e684727d35d6
Matheus Mortari <matheus.felipe.19rt@gmail.com>
2025-11-08 19:00:28 -0300

    chore(ui): remove fullscreen layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,20 @@
 <template>
-  <main class="bg-image min-h-screen">
+  <Navbar />
+
+  <Loading v-if="isLoading" />
+
+  <main v-show="!isLoading" class="flex min-h-screen w-full overflow-x-hidden">
     <slot />
   </main>
 
   <Footer />
 </template>
+
+<script setup lang="ts">
+const isLoading = ref(true)
+
+onMounted(async () => {
+  await nextTick()
+  isLoading.value = false
+})
+</script>
